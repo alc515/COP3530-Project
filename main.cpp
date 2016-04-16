c0a49c8aa208025eee2784c1ade2dc330376c3da
Kyle Walsh <kylegwalsh@ufl.edu>
2016-04-16 14:30:29 -0400

    Updated analyzeMagi nlogn

--- main.cpp
+++ main.cpp
@@ -15,8 +15,10 @@ struct realm{
 	string charm;
 	//number of magi
 	int magi;
-	//possible magi that can be used from the position of the magi
-	int* possibleMagi;
+	//array used to find maximum number of usable magi (LIS)
+	int* S;
+	//array used to find the optimum powers (LIS)
+	int* parent;
 	//the maximum number of magi that can be used
 	int maxMagi;
 	//the values of each magi's power
@@ -25,10 +27,8 @@ struct realm{
 	int* optimumPowers;
 	//number of incantations needed to reach the other realms
 	int* incantationsNeeded;
-	//whether a realm has been visited
-	bool visited;
-//	//whether the realm is capable of reaching the next realm
-//	bool feasible;
+//	//whether a realm has been visited
+//	bool visited;
 };
 
 struct state{
@@ -38,81 +38,110 @@ struct state{
 	int incantationCount;
 };
 
-//finds the optimized powers based on the max number of magi
-void backtrace(realm* r){
-	int k=0;
-	int lastPower=0;
-	int currentPower;
-	int currentPossible=0;
-	int i=0;
-	r->optimumPowers = new int[r->maxMagi];
+//finds the powers that maximize the number of feasible magi
+void findOptimumPowers(realm* r){
+    r->optimumPowers = new int[r->maxMagi];
+    int i=r->maxMagi-1;
+    int index = r->S[i];
 
-	//scan all magi with the same possibility value and pick smallest feasible power
-	while(i<r->magi-1){
-		currentPower=r->powers[i];
-		currentPossible=r->possibleMagi[i];
-		i++;
-		while(i<r->magi-1&&r->possibleMagi[i]==currentPossible){
-			if(r->powers[i]<currentPower&&r->powers[i]>lastPower){
-				currentPower=r->powers[i];
-			}
-			i++;
-		}
-		r->optimumPowers[k]=currentPower;
-		lastPower=r->optimumPowers[k];
-		k++;
-	}
-	//adds last index if that magi was used
-	if(k<r->maxMagi){
-		r->optimumPowers[k]=r->powers[r->magi-1];
-	}
+    while(index!=-1){
+        r->optimumPowers[i]=r->powers[index];
+        index=r->parent[index];
+        i--;
+    }
 }
 
-//analyzes the powers of the magi and finds 1) the max number of magi that can be used 2) the optimized powers based on the max number of magi
+//performs binary search to find location of a value
+int binarySearch(realm* r, int low, int high, int val){
+   int mid;
+
+   //compares high and low value to find target index
+   while(low<high){
+       mid=(high+low)/2;
+
+       //if mid value is less than the value
+       if(r->powers[r->S[mid]]<val){
+           low=mid+1;
+       }
+       //if mid value is greater than the value
+       else if(r->powers[r->S[mid]]>val){
+           high=mid;
+       }
+       //if the mid value is the value
+       else{
+           return mid;
+       }
+   }
+
+   //if index isn't found in the loop then the low index is the target index
+   return low;
+}
+
+//finds longest increasing subsequence of powers (maxMagi)
 void analyzeMagi(realm* r){
-	//sets possible magi for the last position equal to 1 (base case)
-	r->possibleMagi[r->magi-1]=1;
+    //allocates space for arrays
+	r->S = new int[r->magi];
+    r->parent = new int[r->magi];
 
-	//calculates maximum possible magi for a realm using dynamic programming
-	for(int i=r->magi-2; i>=0; i--){
-		//checks values greater than i for previously computed value
-		for(int j=i+1; j<r->magi; j++){
-			if(r->powers[i]<r->powers[j]){
-				r->possibleMagi[i]=r->possibleMagi[j]+1;
-				break;
-			}
-		}
-		//if computed value for possible magi is less than the next index's, then set equal to next index's (max)
-		if(r->possibleMagi[i]<r->possibleMagi[i+1]){
-			r->possibleMagi[i]=r->possibleMagi[i+1];
-		}
-	}
+    //sets initial values
+    int index = 0;
+    int newIndex;
+    r->S[0]=0;
+    r->parent[0]=-1;
 
-	//maxMagi will be the first index of possibleMagi
-	r->maxMagi=r->possibleMagi[0];
-	backtrace(r);
+    for(int i=1 ; i<r->magi ;i++){
+        //if the value is smaller than the previous value - replaces the index of the smallest value greater than or equal to the new value
+        if(r->powers[i]<=r->powers[r->S[index]]){
+            newIndex=binarySearch(r,0,index,r->powers[i]);
+            r->S[newIndex]=i;
+            if(newIndex!=0)
+            {
+            	r->parent[i]=r->S[newIndex-1];
+            }
+            else{
+            	r->parent[i]=-1;
+            }
+        }
+        //if value is larger than the previous value - appends the new index to the end of the current list
+        else{
+            index=index+1;
+            r->S[index]=i;
+            r->parent[i]=r->S[index-1];
+        }
+    }
+    r->maxMagi=index+1;
+
+    //finds the powers for the maximum number of magi
+    findOptimumPowers(r);
+
+    for(int i=0; i<r->maxMagi; i++){
+    	cout << r->optimumPowers[i];
+    }
 }
 
 //finds the distances between strings
-int stringCompare(string string1,string string2){
+int stringCompare(string string1, string string2){
     int size1;
     int size2;
     size1=string1.size()+1;
     size2=string2.size()+1;
 
-    int** myarray=new int* [size1];
-    for (int i=0; i< size1;i++){
-        myarray[i]=new int[size2];
+    //allocates memory for 2-D array to hold differences
+    int** myarray = new int* [size1];
+    for (int i=0; i< size1; i++){
+        myarray[i] = new int[size2];
     }
-    for(int i=0; i< size1; i++){
+
+    for(int i=0; i<size1; i++){
         myarray[i][0]=i;
     }
     for(int i=0; i < size2; i++){
         myarray[0][i]=i;
     }
 
+    //compares the two strings
     int lowest;
-	for(int i=0; i< size1-1;i++){
+	for(int i=0; i<size1-1; i++){
 		for(int j=0; j< size2-1; j++){
 			lowest=myarray[i][j];
 			if (string1.at(i)!=string2.at(j)){
@@ -131,83 +160,70 @@ int stringCompare(string string1,string string2){
 
 	//clears up dynamically allocated memory
 	for(int i=0; i<size1; i++){
-		delete myarray[i];
+		delete[] myarray[i];
 	}
-	delete myarray;
+	delete[] myarray;
 
     return answer;
 }
 
-
-//void greedyAlgorithm(realm* realms, int startRealm, int endRealm, int numRealms){
+//void BFS(realm* realms, int startRealm, int endRealm, int numRealms){
 //	int currentRealm=startRealm;
-//	int nextRealm=startRealm;
 //	int gemCount=0;
 //	int incantationCount=0;
-//	bool impossible=false;
-//	stack<state> stack;
+//	int tempGems;
+//	int tempIncantations;
+//	int minIncantations=0;
+//	int minGems=0;
+//	bool impossible=true;
+//	queue<state> q;
 //	state s;
+//
+//	//pushes intital values onto queue
+//	s.currentRealm=currentRealm;
+//	s.gemCount=gemCount;
+//	s.incantationCount=incantationCount;
+//	q.push(s);
 //
 //	//declares every realm unvisited
 //	for(int i=0; i<numRealms; i++){
 //		realms[i].visited=false;
 //	}
 //
-//	while(nextRealm!=endRealm&&!impossible){
-//		//determines which realms can be reached from the current unvisited realm
-//		if(!realms[currentRealm].visited){
-//			for(int i=0; i<numRealms; i++){
-//				if(realms[currentRealm].incantationsNeeded[i]<=realms[currentRealm].maxMagi){
-//					realms[i].feasible=true;
-//				}
-//				else{
-//					realms[i].feasible=false;
-//				}
-//			}
-//			realms[currentRealm].visited=true;
-//		}
+//	while(!q.empty()){
+//		//gets the front of the queue
+//		s=q.front();
+//		q.pop();
+//		currentRealm=s.currentRealm;
+//		gemCount=s.gemCount;
+//		incantationCount=s.incantationCount;
+//		realms[currentRealm].visited=true;
 //
-//		//if you can reach the end realm from the current realm
-//		if(realms[endRealm].feasible){
-//			nextRealm=endRealm;
-//		}
-//		//if you have to try to pass through other realms
-//		else{
-//			for(int i=0; i<numRealms; i++){
-//				if(realms[i].feasible&&realms[i].incantationsNeeded[endRealm]<=realms[nextRealm].incantationsNeeded[endRealm]){
-//					nextRealm=i;
-//				}
-//			}
-//		}
-//
-//		//if unable to find a feasible nextRealm
-//		if(currentRealm==nextRealm){
-//			impossible=true;
-//		}
-//
-//		if(!impossible){
-//			s.currentRealm=currentRealm;
-//			s.nextRealm=nextRealm;
-//			s.incantationCount=incantationCount;
-//			s.gemCount=gemCount;
-//			stack.push(s);
-//			//gets the number of gems and incantations needed to move to the next realm
-//			for(int i=0; i<realms[currentRealm].incantationsNeeded[nextRealm]; i++){
-//				gemCount+=realms[currentRealm].optimumPowers[i];
-//				incantationCount++;
-//			}
-//			//the current realm is now the next realm
-//			currentRealm=nextRealm;
-//		}
-//		else if(!stack.empty()){
-//			s=stack.top();
-//			stack.pop();
-//			currentRealm=s.currentRealm;
-//			nextRealm=currentRealm;
-//			gemCount=s.gemCount;
-//			incantationCount=s.incantationCount;
-//			realms[s.nextRealm].feasible=false;
+//		//if the currentRealm is the endRealm
+//		if(currentRealm==endRealm&&(minIncantations==0||incantationCount<minIncantations)){
 //			impossible=false;
+//			minGems=gemCount;
+//			minIncantations=incantationCount;
+//			continue;
+//		}
+//
+//		//determines which realms can be reached from the current realm
+//		for(int i=0; i<numRealms; i++){
+//			if(!realms[i].visited&&realms[currentRealm].incantationsNeeded[i]<=realms[currentRealm].maxMagi){
+//				//resets gems and incantations back to current realms values
+//				tempGems=gemCount;
+//				tempIncantations=incantationCount;
+//				//gets the number of gems and incantations needed to move to the next realm
+//				for(int j=0; j<realms[currentRealm].incantationsNeeded[i]; j++){
+//					tempGems+=realms[currentRealm].optimumPowers[j];
+//					tempIncantations++;
+//				}
+//				//pushes new realms' values into queue
+//				s.gemCount=tempGems;
+//				s.incantationCount=tempIncantations;
+//				s.currentRealm=i;
+//				q.push(s);
+//			}
 //		}
 //	}
 //
@@ -216,78 +232,9 @@ int stringCompare(string string1,string string2){
 //		cout << "IMPOSSIBLE\n";
 //	}
 //	else{
-//		cout << incantationCount << " " << gemCount << endl;
+//		cout << minIncantations << " " << minGems << endl;
 //	}
 //}
-
-void BFS(realm* realms, int startRealm, int endRealm, int numRealms){
-	int currentRealm=startRealm;
-	int gemCount=0;
-	int incantationCount=0;
-	int tempGems;
-	int tempIncantations;
-	int minIncantations=0;
-	int minGems=0;
-	bool impossible=true;
-	queue<state> q;
-	state s;
-
-	//pushes intital values onto queue
-	s.currentRealm=currentRealm;
-	s.gemCount=gemCount;
-	s.incantationCount=incantationCount;
-	q.push(s);
-
-	//declares every realm unvisited
-	for(int i=0; i<numRealms; i++){
-		realms[i].visited=false;
-	}
-
-	while(!q.empty()){
-		//gets the front of the queue
-		s=q.front();
-		q.pop();
-		currentRealm=s.currentRealm;
-		gemCount=s.gemCount;
-		incantationCount=s.incantationCount;
-		realms[currentRealm].visited=true;
-
-		//if the currentRealm is the endRealm
-		if(currentRealm==endRealm&&(minIncantations==0||incantationCount<minIncantations)){
-			impossible=false;
-			minGems=gemCount;
-			minIncantations=incantationCount;
-			continue;
-		}
-
-		//determines which realms can be reached from the current realm
-		for(int i=0; i<numRealms; i++){
-			if(!realms[i].visited&&realms[currentRealm].incantationsNeeded[i]<=realms[currentRealm].maxMagi){
-				//resets gems and incantations back to current realms values
-				tempGems=gemCount;
-				tempIncantations=incantationCount;
-				//gets the number of gems and incantations needed to move to the next realm
-				for(int j=0; j<realms[currentRealm].incantationsNeeded[i]; j++){
-					tempGems+=realms[currentRealm].optimumPowers[j];
-					tempIncantations++;
-				}
-				//pushes new realms' values into queue
-				s.gemCount=tempGems;
-				s.incantationCount=tempIncantations;
-				s.currentRealm=i;
-				q.push(s);
-			}
-		}
-	}
-
-	//prints the number of incantations and gems if possible
-	if(impossible){
-		cout << "IMPOSSIBLE\n";
-	}
-	else{
-		cout << minIncantations << " " << minGems << endl;
-	}
-}
 
 void messenger(){
 	//gets the number of realms and creates an array to store them in
@@ -303,12 +250,10 @@ void messenger(){
 		//gets number of magi
 		cin >> realms[i].magi;
 
-		//gets powers of each realm's magi/initializes possible magi
+		//gets powers of each realm's magi
 		realms[i].powers = new int[realms[i].magi];
-		realms[i].possibleMagi = new int[realms[i].magi];
 		for(int j=0; j<realms[i].magi; j++){
 			cin >> realms[i].powers[j];
-			realms[i].possibleMagi[i]=1;
 		}
 	}
 
@@ -348,16 +293,17 @@ void messenger(){
 		}
 	}
 
-	//finds incantations and gems to reach end realm
-	BFS(realms, startRealm, endRealm, numRealms);
-
-	//finds incantations and gems to return to start realm
-	BFS(realms, endRealm, startRealm, numRealms);
+//	//finds incantations and gems to reach end realm
+//	BFS(realms, startRealm, endRealm, numRealms);
+//
+//	//finds incantations and gems to return to start realm
+//	BFS(realms, endRealm, startRealm, numRealms);
 
 	//deletes dynamically allocated memory
 	for(int i=0; i<numRealms; i++){
 		delete[] realms[i].powers;
-		delete[] realms[i].possibleMagi;
+		delete[] realms[i].S;
+		delete[] realms[i].parent;
 		delete[] realms[i].optimumPowers;
 		delete[] realms[i].incantationsNeeded;
 	}
